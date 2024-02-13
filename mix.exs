e59786683fba8b84fdfa71b386332cbf651e587b
ImNotAVirus <17680522+ImNotAVirus@users.noreply.github.com>
2024-02-13 20:00:14 +0100

    :fire: Remove unused logger

--- mix.exs
+++ mix.exs
@@ -13,7 +13,7 @@ defmodule StdResult.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: []
     ]
   end
 
